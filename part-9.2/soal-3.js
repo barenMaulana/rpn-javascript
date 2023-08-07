5bcfa8e7b09ac505aeee0bb76e3a685f4b573b4b
baren maulana <barenmaulana@gmail.com>
2023-08-07 16:44:43 +0700

    push

--- part-9.2/soal-3.js
+++ part-9.2/soal-3.js
@@ -1,5 +1,72 @@
 function cariMedian(arr) {
   // you can only write your code here!
+
+  // sorting
+  let sorted = false;
+  let sorted_data = [];
+  let index = 0;
+  let max = 0;
+  while (!sorted) {
+    if (sorted_data.length == 0) {
+      max = arr[index] < arr[index + 1] ? arr[index + 1] : arr[index];
+      sorted_data.push(
+        arr[index] < arr[index + 1] ? arr[index] : arr[index + 1]
+      );
+    } else if (index > 1) {
+      if (arr[index] == undefined) {
+        sorted_data.push(max);
+      } else {
+        sorted_data.push(arr[index] > max ? max : arr[index]);
+        max = arr[index] > max ? arr[index] : max;
+      }
+    } else if (sorted_data.length == arr.length) {
+      // checking apakah all sorted
+      let expected_result = sorted_data.length;
+      let check_result = 1;
+      for (let i = 1; i < sorted_data.length; i++) {
+        if (sorted_data[i - 1] <= sorted_data[i]) {
+          check_result++;
+        }
+        if (check_result == expected_result) {
+          sorted = true;
+          break;
+        }
+      }
+
+      // sorting lagi sisanya
+      if (!sorted) {
+        for (let i = 0; i < sorted_data.length; i++) {
+          let temp = sorted_data[i];
+          if (
+            sorted_data[i] >= sorted_data[i + 1] &&
+            sorted_data[i + 1] != undefined
+          ) {
+            sorted_data[i] = sorted_data[i + 1];
+            sorted_data[i + 1] = temp;
+          }
+        }
+      }
+    }
+    if (index == arr.length) {
+      index = 0;
+    }
+    index++;
+  }
+
+  // check ganjil atau genap
+  let median = 0;
+  if (sorted_data.length % 2 != 0) {
+    median = sorted_data.length / 2;
+    median = Math.floor(median);
+    median = sorted_data[median];
+  } else {
+    median =
+      (sorted_data[Math.floor(sorted_data.length / 2) - 1] +
+        sorted_data[Math.ceil(sorted_data.length / 2)]) /
+      2;
+  }
+
+  return median;
 }
 
 // TEST CASES
---
+++ part-9.2/soal-5.js
@@ -0,0 +1,51 @@
+//sistem ubah hurufnya misal huruf a diubah menjadi b, c menjadi d, b menjadi c, z menjadi a
+//intinya ubah huruf menjadi huruf setelahnya
+function ubahHuruf(kata) {
+  // you can only write your code here!
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  let new_word = "";
+  for (let j = 0; j < kata.length; j++) {
+    for (let i = 0; i < alphabet.length; i++) {
+      if (kata[j] == alphabet[i]) {
+        new_word += alphabet[i + 1];
+      }
+    }
+  }
+
+  return new_word;
+}
+
+// TEST CASES
+console.log(ubahHuruf("wow")); // xpx
+console.log(ubahHuruf("developer")); // efwfmpqfs
+console.log(ubahHuruf("javascript")); // kbwbtdsjqu
+console.log(ubahHuruf("keren")); // lfsfo
+console.log(ubahHuruf("semangat")); // tfnbohbu

845be29e753f98f529ab2dff5847cc754f367b55
user <user@users-MacBook-Pro.local>
2023-08-07 08:31:25 +0700

    push

---
+++ part-9.2/soal-2.js
@@ -0,0 +1,19 @@
+//cari faktor persekutuan terbesar
+function fpb(angka1, angka2) {
+  // you can only write your code here!
+  while (angka1 > 0 && angka2 > 0) {
+    if (angka1 > angka2) {
+      angka1 = angka1 % angka2;
+    } else if (angka1 < angka2) {
+      angka2 = angka2 % angka1;
+    }
+  }
+  return angka1 + angka2;
+}
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
